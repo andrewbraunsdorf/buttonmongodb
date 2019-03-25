354c2af0a85775980a18f824e0e5adfb8b14707e
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-25 20:01:06 +0000

    required mongoclient and added database

--- server.js
+++ server.js
@@ -1,15 +1,30 @@
 console.log('Server side is running');
 
 const express = require('express');
-
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 // serve files from the public directory
 app.use(express.static('public'));
 
+let db;
+
+const url =  'mongodb://localhost:8080/clicks';
+
 // start the express web server listening on 8080
 app.listen(8080, () => {
   console.log('listening on 8080');
+});
+
+MongoClient.connect(url, (err, database) => {
+  if(err) {
+    return console.log(err);
+  }
+  db = database;
+  // start the express web server listening on 8080
+  app.listen(8080, () => {
+    console.log('listening on 8080');
+  });
 });
 
 // serve the homepage
