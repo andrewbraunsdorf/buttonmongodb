82f094a2b89c188bf08eb2d3578e5e49c924ea02
Andrew Braunsdorf <andrewbraunsdorf.student@careerdevs.com>
2019-03-28 01:03:02 +0000

    added collection to save clicks

--- server.js
+++ server.js
@@ -36,4 +36,12 @@ app.post('/clicked', (req, res) => {
   const click = {clickTime: new Date()};
   console.log(click);
   console.log(db);
+
+  db.collection('clicks').save(click, (err, result) => {
+    if (err) {
+      return console.log(err);
+    }
+    console.log('click added to db');
+    res.sendStatus(201);
+  });
 });
